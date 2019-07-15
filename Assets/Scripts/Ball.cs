fb7d0463c70197fa8925fb2c1119d1ec50a8322e
Zombzu <parker.azra@gmail.com>
2019-07-15 10:24:30 +0200

    Added Scripts

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    [SerializeField] Paddle padde1;
+    [SerializeField] float velocityX = 2f;
+    [SerializeField] float velocityY = 15f;
+    bool hasStarted = false;
+
+    Vector2 PaddleToBallVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PaddleToBallVector = transform.position - padde1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBalltPaddle();
+        }
+        LaunchOnClick();
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX,velocityY);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBalltPaddle()
+    {
+        Vector2 PaddlePos = new Vector2(padde1.transform.position.x, padde1.transform.position.y);
+        transform.position = PaddlePos + PaddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    [SerializeField] float screenWidth = 16f;
+    [SerializeField] public float beginX = 1f;
+    [SerializeField] public float endX = 15f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        float MousePos = (Input.mousePosition.x / Screen.width)*screenWidth;
+        Vector2 PaddlePos = new Vector2(transform.position.x, transform.position.y);
+        PaddlePos.x = Mathf.Clamp(MousePos, beginX, endX);
+        transform.position = PaddlePos;
+	}
+}
